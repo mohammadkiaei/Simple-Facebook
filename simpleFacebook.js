a7a217e161c7c08658a3bf3e7c7cb8222d21a50d
mohammadkiaei <s.kiaei.s.m.m.k1380@gmail.com>
2022-11-16 10:50:23 +0330

    Initial commit

---
+++ simpleFacebook.js
@@ -0,0 +1,51 @@
+var database = [{
+		username: "andrei",
+		password: "supersecret"
+	},
+	{
+		username: "sally",
+		password: "123"
+	},
+	{
+		username: "ingrid",
+		password: "777"
+	}
+];
+
+var newsfeed = [{
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeetyy cool!"
+	}
+];
+
+var userNamePrompt = prompt("What's your username?");
+var PasswordPrompt = prompt("What's your password?");
+
+function isUserValid(username, password) {
+	for (var i = 0; i < database.length; i++) {
+		if (database[i].username === username && database[i].password === password) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function signIn(username, password) {
+	if (isUserValid(username, password)) {
+		console.log(newsfeed);
+	} else {
+		alert("Sorry, wrong username and password!")
+	}
+}
+
+signIn(userNamePrompt, PasswordPrompt);
+
+// Developed By SeyedMohammadMahdi Kiaei
